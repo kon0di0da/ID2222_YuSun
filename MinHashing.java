da032c0593a601a348a4cddaa6812f78e65a6228
kon0di0da <91859953+kon0di0da@users.noreply.github.com>
2023-11-11 13:55:47 +0100

    Add files via upload

--- MinHashing.java
+++ MinHashing.java
@@ -3,51 +3,80 @@ package LSH;
 import java.util.*;
 
 public class MinHashing {
+    private static class HashFunction {
+        private final int a;
+        private final int b;
 
-    public List<List<Integer>> generateSignatureMatrix(List<Set<Integer>> sets, int n) {
-        List<List<Integer>> signatureMatrix = new ArrayList<>();
-
-        for (Set<Integer> hashedShingles : sets) {
-            Set<Integer> minHashSignature = buildMinHashSignature(hashedShingles, n);
-            signatureMatrix.add(new ArrayList<>(minHashSignature));
+        public HashFunction(int a, int b) {
+            this.a = a;
+            this.b = b;
         }
 
-        return signatureMatrix;
+        public int evaluate(int x) {
+            return (int) (((long) a * x + b) & 0xFFFFFFFFL); // Use bitwise AND to get an unsigned int
+        }
+    }
+    public List<List<Integer>> buildMinHashSignature(List<Set<Integer>> hashedShinglesGroup,int n) {
+        // Ensure that n is not greater than the size of the hashedShingles set
+        n = Math.min(n, hashedShinglesGroup.get(0).size());
+
+        // Generate random hash functions (represented as simple linear functions)
+        List<HashFunction> hashFunctions = generateHashFunctions(n);
+
+        // Initialize minHash signature with positive infinity for each hash function
+
+        List<List<Integer>> res =new ArrayList<>();
+        // Iterate through each hashed shingle
+        for (Set<Integer> hashedShingles : hashedShinglesGroup) {
+            Integer[] minHashSignature = new Integer[n];
+            Arrays.fill(minHashSignature, Integer.MAX_VALUE);
+            for (Integer hashedShingle : hashedShingles) {
+                // Evaluate each hash function and update the minHash signature
+                for (int i = 0; i < n; i++) {
+                    minHashSignature[i] = Math.min(minHashSignature[i], hashFunctions.get(i).evaluate(hashedShingle));
+                }
+            }
+            res.add(Arrays.asList(minHashSignature));
+        }
+
+
+        // Convert the minHash signature array to a set
+        return res;
     }
 
-    private Set<Integer> buildMinHashSignature(Set<Integer> hashedShingles, int n) {
-        // Your existing MinHashing logic here...
-
-        // For simplicity, I'll use a placeholder implementation
+    private List<HashFunction> generateHashFunctions(int n) {
+        // Generate random hash functions (simple linear functions)
+        List<HashFunction> hashFunctions = new ArrayList<>();
         Random random = new Random();
-        Set<Integer> minHashSignature = new HashSet<>();
 
         for (int i = 0; i < n; i++) {
-            minHashSignature.add(random.nextInt(Integer.MAX_VALUE));
+            int a = random.nextInt(Integer.MAX_VALUE);
+            int b = random.nextInt(Integer.MAX_VALUE);
+            hashFunctions.add(new HashFunction(a, b));
         }
 
-        return minHashSignature;
+        return hashFunctions;
     }
 
-//    public static void main(String[] args) {
-//        // Example usage
-//        MinHashing minHashing = new MinHashing();
-//
-//        // Example list of integer sets (hashed shingles)
-//        List<Set<Integer>> sets = new ArrayList<>();
-//        sets.add(Set.of(1, 2, 3, 4, 5));
-//        sets.add(Set.of(3, 4, 5, 6, 7));
+    public static void main(String[] args) {
+        // Example usage
+        MinHashing minHashing = new MinHashing();
+
+        // Example list of integer sets (hashed shingles)
+        List<Set<Integer>> sets = new ArrayList<>();
+        sets.add(Set.of(1, 2, 3, 4, 5));
+        sets.add(Set.of(1,2,3,4,5));
 //        sets.add(Set.of(2, 3, 5, 8, 9));
-//
-//        // Specify the length of the minHash signatures
-//        int n = 3;
-//
-//        // Generate and print the minHash signature matrix
-//        List<List<Integer>> signatureMatrix = minHashing.generateSignatureMatrix(sets, n);
-//        System.out.println("MinHash Signature Matrix:");
-//        for (List<Integer> row : signatureMatrix) {
-//            System.out.println(row);
-//        }
-//    }
+
+        // Specify the length of the minHash signatures
+        int n = 3;
+
+        // Generate and print the minHash signature matrix
+        List<List<Integer>> signatureMatrix = minHashing.buildMinHashSignature(sets, n);
+        System.out.println("MinHash Signature Matrix:");
+        for (List<Integer> row : signatureMatrix) {
+            System.out.println(row);
+        }
+    }
 }
 
